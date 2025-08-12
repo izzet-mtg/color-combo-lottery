821fd7eeb7545402893ccff1ce2818a43653f7f9
moba1 <moba2@protonmail.com>
2025-08-13 00:33:45 +0900

    エラーがあったときにダイアログ表示するようにした

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,11 +3,25 @@
 
 import Image from "next/image";
 import Lottery from "@/app/component/Lottery";
+import { useState } from "react";
+import ErrorDialog from "../component/ErrorDialog";
 
 export default function Home() {
+  const [error, setError] = useState<any>();
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <Lottery />
+      <Lottery onError={setError}/>
+      {error && (
+        <ErrorDialog onClose={() => setError(undefined)}>
+          <p>以下の情報を連絡先から管理者に送信してください</p>
+          <p>(連絡先: {process.env.NEXT_PUBLIC_CONTACT_LINK})</p>
+          <div className='p-4'>
+            <div className="bg-gray-100 text-gray-800 text-sm p-4 rounded-md border border-gray-300 overflow-x-auto whitespace-pre-wrap">
+              <p className='font-bold'>エラー内容: {`${error}`}</p>
+            </div>
+          </div>
+        </ErrorDialog>
+      )}
     </main>
   );
 }
