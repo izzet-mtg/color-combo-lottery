05f9ba929d69187cdda2e421187cf17a8c4b1247
moba1 <moba2@protonmail.com>
2025-08-13 00:15:04 +0900

    ボタンの見た目をroundedに

--- src/app/component/Lottery.tsx
+++ src/app/component/Lottery.tsx
@@ -16,11 +16,16 @@ export const Lottery = ({ onClick }: LotteryProps) => {
   return (
     <div>
       {symbols && (
-        <div className="p-8">
-          <div className="flex gap-1">
-            {combo.map((symbol, index) => <img key={`color-symbol${index}`} src={symbols[symbol]} />)}
+        <div className="p-8 flex justify-center flex-col gap-8">
+          <div className="flex gap-1 justify-center">
+            {combo.map((symbol, index) => <img key={`color-symbol${index}`} src={symbols[symbol]} className="w-16" />)}
           </div>
-          <button onClick={() => { const combo = chooseCombo(); setCombo(combo); onClick?.(combo); }}>再選択</button>
+          <button
+            className="rounded-full px-6 py-3 bg-blue-500 hover:bg-blue-700 text-white font-medium shadow-lg max-w-xs mx-auto"
+            onClick={() => { const combo = chooseCombo(); setCombo(combo); onClick?.(combo); }}
+          >
+            再選択
+          </button>
         </div>
       )}
     </div>
