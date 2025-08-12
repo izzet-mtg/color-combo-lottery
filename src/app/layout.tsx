952823bc9eef9872672ae63b4f899e46e58ad15a
moba1 <moba2@protonmail.com>
2025-08-13 00:35:03 +0900

    フォントを Noto Sans JP に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,12 @@
 // SPDX-License-Identifier: MIT
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSansJp = Noto_Sans_JP({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
 });
 
 export const metadata: Metadata = {
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSansJp.variable} antialiased`}
       >
         <div className="w-full">
           {children}
