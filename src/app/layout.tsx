09588c1d6a3e155d28c417183bd6a5cc85822fbb
moba1 <moba2@protonmail.com>
2025-08-14 00:45:36 +0900

    html の lang を ja に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${notoSansJp.variable} antialiased flex flex-col min-h-screen`}
       >
