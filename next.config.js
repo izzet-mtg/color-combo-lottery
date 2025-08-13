e5eb96cda591b3d383be626f578d0fa243db1dc1
moba1 <moba2@protonmail.com>
2025-08-13 14:24:30 +0900

    basePath と assetPrefix を使うようにした

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 
 // GitHub Pages は独自ドメインを使わずに素で使うと `https://<org 名>.github.io/<リポジトリー名>/<ページ URL>` になる
 // つまり、 `/` で飛んでしまうと `https://<org 名>.github.io/` に飛んでしまうが、本来飛びたいのは `https://<org 名>.github.io/<リポジトリー名>/` なはず
-// そのため、苦肉の策として production 環境である GitHub Pages 環境であれば URL に `/<リポジトリー名>` をくっつけるようにする。
+// そのため、 production 環境である GitHub Pages 環境であれば URL に `/<リポジトリー名>` をくっつけるようにする。
 const basePath = process.env.NODE_ENV === "production" ? "/color-combo-lottery" : "";
 
 /** @type {import('next').NextConfig} */
@@ -11,8 +11,6 @@ const nextConfig = {
   output: "export",
   env: {
     NEXT_PUBLIC_CONTACT_LINK: "https://forms.gle/2jK87DQ44CS2pfBR9",
-    // 各種リンク参照はこの環境変数を使い回して頑張る
-    NEXT_PUBLIC_BASE_PATH: basePath,
     NEXT_PUBLIC_GITHUB_REPOSITORY_URL: "https://github.com/izzet-mtg/color-combo-lottery/",
     NEXT_PUBLIC_SITE_URL: "https://izzet-mtg.github.io/color-combo-lottery",
   },
@@ -20,4 +18,4 @@ const nextConfig = {
   basePath: basePath,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,13 +1,17 @@
 // Copyright (c) 2025 izzet-mtg
 // SPDX-License-Identifier: MIT
 
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/asset/logo.svg";
+
 const Header = () =>  (
   <header className="flex flex-col flex-wrap w-full dark:bg-gray-800 bg-gray-600 p-4 gap-2">
     <div>
       {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-      <a href={`${process.env.NEXT_PUBLIC_BASE_PATH}/`} className="inline-block">
-        <img src={`${process.env.NEXT_PUBLIC_BASE_PATH}/logo.svg`} alt="ホームへ" width="50" height="50" />
-      </a>
+      <Link href="/" className="inline-block">
+        <Image src={Logo} alt="ホームへ" width="50" height="50" />
+      </Link>
     </div>
   </header>
 );
